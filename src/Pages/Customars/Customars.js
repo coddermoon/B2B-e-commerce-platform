73400d3ca4b7b4be1f4dea52e0e88c32b9e79ac9
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-09 11:37:07 +0600

    fake data of users added

--- src/Pages/Customars/Customars.js
+++ src/Pages/Customars/Customars.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CustomarCard from '../../components/CustomarCard';
 
 const Customars = () => {
+    const [users,setUsers]= useState([])
+
+    useEffect(() => {
+
+        fetch('./fakedata/users.json')
+        .then(response => response.json())
+        .then(data=>console.log(data))
+        // axios.get('fakedata/users.json')
+        //    .then(response => {
+        //         setUsers(response.data);
+        //     })
+    },[])
+    console.log(users)
     return (
         <div className='mt-5 py-8'>
-<div className="rounded-md shadow-xl py-5  w-3/2 bg-white text-gray-900 border ">
-	
-<div className="grid grid-cols-12 justify-center items-center ">
-<div className="img-user col-span-2 ml-3">
-<img src="https://source.unsplash.com/50x50/?portrait" alt="" className="object-cover object-center w-20 h-20 rounded shadow-sm dark:bg-gray-500 dark:border-gray-700" />
-</div>
-{/* name section */}
 
-<div className="col-span-8 flex flex-col md:flex-row justify-around items-center">
-    <h2 className='text-2xl font-semibold text-center'>Mahamodul Hasan Moon</h2>
-    <h5 className='font-semibold text-gray-800 text-center'>40 orders</h5>
-</div>
-<div>
+            {
+            users.map(user => <CustomarCard
+            key={user._id}
+            user={user}
+            />)
+            }
 
-</div>
-
-</div>
-	
-</div>
-
+<CustomarCard/>
 
 
         </div>
---
+++ src/components/CustomarCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CustomarCard = ({user}) => {
+    const {name,imageUrl,orders}= user
+    return (
+        <div className="rounded-md shadow-xl py-5  w-3/2 bg-white text-gray-900 border ">
+	
+        <div className="grid grid-cols-12 justify-center items-center ">
+        <div className="img-user col-span-2 ml-3">
+        <img src={imageUrl} alt="" className="object-cover object-center w-20 h-20 rounded shadow-sm dark:bg-gray-500 dark:border-gray-700" />
+        </div>
+        {/* name section */}
+        
+        <div className="col-span-8 flex flex-col md:flex-row justify-around items-center">
+            <h2 className='text-2xl font-semibold text-center'>{name}</h2>
+            <h5 className='font-semibold text-gray-800 text-center'>{orders} orders</h5>
+        </div>
+        <div>
+        
+        </div>
+        
+        </div>
+            
+        </div>
+    );
+};
+
+export default CustomarCard;
