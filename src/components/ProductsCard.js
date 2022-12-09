4e7bd3ca83649628d114691dd61eacfb28d5e72e
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-09 21:42:14 +0600

    done to product card page

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProductsCard = ({product}) => {
 
-    const {productName,productTumb}= product
+    const {productName,productTumb,_id}= product
 
   return (
     <div className="rounded-md shadow-xl py-5 my-3   mx-auto bg-white text-gray-900 border ">
@@ -17,7 +17,7 @@ const ProductsCard = ({product}) => {
         {/* name section */}
 
         <div className="col-span-3  ml-12">
-          <h2 className="text-2xl font-semibold text-left">Ice Cream</h2>
+          <h2 className="text-2xl font-semibold text-left">{productName}</h2>
           <h5 className="font-semibold text-gray-800 text-xl text-left ">
             price : 55 $
           </h5>
@@ -26,12 +26,12 @@ const ProductsCard = ({product}) => {
 
             <div>
               <label
-                for={`Toggle${2}`}
+                for={`Toggle${_id}`}
                 className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100"
               >
                 <span>Left</span>
                 <span className="relative">
-                  <input id={`Toggle${2}`} type="checkbox" className="hidden peer" />
+                  <input id={`Toggle${_id}`} type="checkbox" className="hidden peer" />
                   <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
                   <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
                 </span>
