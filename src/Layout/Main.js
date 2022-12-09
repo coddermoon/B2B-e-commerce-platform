e9a932fafbc5b28337055f59733171d62867a77d
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-09 13:27:17 +0600

    done searchbox section

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchForm from '../components/SearchForm';
 import Customars from '../Pages/Customars/Customars';
 
 const Main = () => {
     return (
         <div className='container mx-auto'>
+
+            <SearchForm/>
          <Customars/>
         </div>
     );
